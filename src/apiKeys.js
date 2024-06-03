c9220dcf9c937a972cf0786ade04bdcb1cdbe578
Pratham Raj <pratham55rajbly@gmail.com>
2024-06-03 14:49:29 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "34dfeda4a2cf9c967d12d45f33c37035",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
